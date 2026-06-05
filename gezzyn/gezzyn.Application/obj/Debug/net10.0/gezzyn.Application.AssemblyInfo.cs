e2c97483dacc0e48377cb36414a33bf84b73aa97
nagihanustun <110236705+nagihanustun@users.noreply.github.com>
2026-06-05 14:41:35 +0300

    Remove Visual Studio cache files

--- gezzyn/gezzyn.Application/obj/Debug/net10.0/gezzyn.Application.AssemblyInfo.cs
+++ gezzyn/gezzyn.Application/obj/Debug/net10.0/gezzyn.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gezzyn.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af3b5e600323ae4a3667c1189a79ed52ef984e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("gezzyn.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gezzyn.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- gezzyn/gezzyn.Infrastructure/obj/Debug/net10.0/gezzyn.Infrastructure.AssemblyInfo.cs
+++ gezzyn/gezzyn.Infrastructure/obj/Debug/net10.0/gezzyn.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gezzyn.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8af3b5e600323ae4a3667c1189a79ed52ef984e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("gezzyn.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gezzyn.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
